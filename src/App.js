7d970161326cea959ff8b87e9f8660fef86565bf
Huzaif Khan <129494101+Huzaifkhan537@users.noreply.github.com>
2023-10-28 16:30:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import MainDash from './component/MainDash/MainDash';
+import RightSide from './component/RightSide/RightSide';
+import Sidebar from './component/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar />
+        <MainDash/>
+        <RightSide/>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Updates/Updates.jsx
@@ -0,0 +1,29 @@
+import react from 'react'
+import './Updates.css'
+import { UpdatesData } from '../../Data/Data'
+
+const Updates = () => {
+    return (
+        <div className="Updates">
+            {UpdatesData.map((update) => {
+                return (
+                    <div className="update">
+                        <img src="{update.img}" alt="" />
+                        <div className="noti">
+                            <div style={{ marginBottom: '0.5rem' }}>
+                                <span>{update.name}</span>
+                                <span>{update.noti}</span>
+
+                            </div>
+
+                            <span>{update.timee}</span>
+                        </div>
+
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Updates 
